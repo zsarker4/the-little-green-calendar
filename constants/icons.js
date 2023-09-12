d72fef3d93738b1206788c8459af8846cb0b96a2
zahra <90015662+zsarker4@users.noreply.github.com>
2023-09-12 15:21:03 -0700

    Add files via upload

---
+++ constants/icons.js
@@ -0,0 +1,31 @@
+import bigRed from "../assets/icons/bigRed.png";
+import blackToggle from "../assets/icons/blackToggle.png";
+import greenBtn from "../assets/icons/greenBtn.png";
+import greenSquare from "../assets/icons/greenSquare.png";
+import leaf from "../assets/icons/leaf.png";
+import lightGreenBtn from "../assets/icons/lightGreenBtn.png";
+import list from "../assets/icons/list.png";
+import smallRed from "../assets/icons/smallRed.png";
+import sprout from '../assets/icons/sprout.png'
+import sunset from '../assets/icons/sunset.png'
+import whiteToggle from '../assets/icons/whiteToggle.png'
+import setting from '../assets/icons/setting.png'
+import gray from '../assets/icons/gray.png'
+import bigGreen from '../assets/icons/bigGreen.png'
+
+export default {
+  bigRed,
+  blackToggle,
+  greenBtn,
+  greenSquare,
+  leaf,
+  lightGreenBtn,
+  list,
+  smallRed,
+  sprout,
+  sunset,
+  whiteToggle,
+  setting,
+  gray,
+  bigGreen
+};
---
+++ constants/theme.js
@@ -0,0 +1,31 @@
+const COLORS = {
+    primary: "#00A86B",
+    secondary: "#0E100F",
+    tertiary: "#AFAFAF",
+  
+    gray: "#7E8491",
+    gray2: "#8F9BB3",
+  
+    // white: "#F3F4F8",
+     lightWhite: "#FFF",
+  };
+  
+  const FONT = {
+    regular: "JakartaReg",
+    medium: "JakartaMedium",
+    semiBold: "JakartaSemiBold",
+    bold: "JakartaBold",
+    italicBold: "JakartaBoldItalic",                                                                                                          
+  };
+  
+  const SIZES = {
+    xSmall: 10,
+    small: 11,
+    medium: 14,
+    large: 18,
+    xLarge: 20,
+    xxLarge: 32,
+  };
+  
+  
+  export { COLORS, FONT, SIZES };
